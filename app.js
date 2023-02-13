b5cd8c0a362c0eba899ed5f26fb920edf3795b27
Noelle-KH <noelletang86@gmail.com>
2023-02-13 08:59:32 +0800

    feat: add handlebars and pages view

--- app.js
+++ app.js
@@ -3,11 +3,32 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 const express = require('express')
+const exphbs = require('express-handlebars')
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
-  res.send('<h1>Hello</h1>')
+  res.render('index')
+})
+
+app.get('/records/new', (req, res) => {
+  res.render('new')
+})
+
+app.get('/records/edit', (req, res) => {
+  res.render('edit')
+})
+
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
 })
 
 app.listen(PORT, () => {
