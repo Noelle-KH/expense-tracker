5e560e42a8de4fbdb7131d12200ff36d39319481
Noelle-KH <noelletang86@gmail.com>
2023-02-13 16:28:03 +0800

    feat: add login logout and register feature

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const exphbs = require('express-handlebars')
 const session = require('express-session')
+const flash = require('connect-flash')
 
 const app = express()
 require('./config/mongoose')
 const passport = require('./config/passport')
+const routes = require('./routes')
 const PORT = process.env.PORT || 3000
 
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
@@ -19,8 +21,17 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }))
+app.use(flash())
 app.use(passport.initialize())
 app.use(passport.session())
+app.use((req, res, next) => {
+  res.locals.user = req.user
+  res.locals.isAuthenticate = req.isAuthenticated()
+  res.locals.success_message = req.flash('success_message')
+  res.locals.warning_message = req.flash('warning_message')
+  next()
+})
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
