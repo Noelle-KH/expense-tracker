b275808c8898c98dae93c429346685a4c1845a2d
Noelle-KH <noelletang86@gmail.com>
2023-02-15 19:08:24 +0800

    refactor: modify pages layout

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.engine('handlebars', exphbs.engine({
   }
 }))
 app.set('view engine', 'handlebars')
+app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 app.use(session({
